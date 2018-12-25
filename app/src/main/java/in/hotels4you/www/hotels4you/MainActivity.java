358410b4c3f0ecc35d34d47f0907a0bc9174fbac
jitunayak <jitunayak715@gmail.com>
2018-12-25 12:44:26 +0530

    initial

---
+++ app/src/main/java/in/hotels4you/www/hotels4you/MainActivity.java
@@ -0,0 +1,13 @@
+package in.hotels4you.www.hotels4you;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
